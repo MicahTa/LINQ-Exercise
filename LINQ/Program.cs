f728da31d1a0cabaf3fdf4af8949fd0fd1c0ac30
Laptop <micahcorbintanner@gmail.com>
2025-05-13 11:07:29 -0600

    Did assignment

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace LINQ
+﻿using System.Runtime.Versioning;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> games = new List<string> {"Minecraft", "Tetris", "Doom", "Pacman", "Portal", "Call Of Duty", "Fortnight"};
+            var orderd = games.OrderByDescending(x => x.Length).Reverse();
+            foreach (string game in orderd) {
+                Console.WriteLine(game);
+            }
         }
     }
 }
